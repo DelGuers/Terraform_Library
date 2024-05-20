d9ce29ad334129d853a01d2e7cd4b3783f0f7ad9
Steve Del <stevildg@gmail.com>
2024-05-20 19:26:17 -0400

    Add ASG, TG, Load Balancer to code

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 #OUTPUT VARIABLES
-output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "The public IP of the Instance"
+output "alb_dns_name" {
+  value       = aws_lb.example.dns_name
+  description = "The DNS name of the load balancer"
 }
