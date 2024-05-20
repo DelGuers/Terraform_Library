68bd58ef8b557c96439783883823647b74b5d82e
Steve Del <stevildg@gmail.com>
2024-05-20 15:06:31 -0400

    Add input variables to main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,51 @@
+#PROVIDERS
 provider "aws" {
   region = "us-east-2"
 }
 
+#RESOURCES
 resource "aws_instance" "example" {
-  ami           = "ami-0fb653ca2d3203ac1"
-  instance_type = "t3.micro"
+  ami                    = "ami-0fb653ca2d3203ac1"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, Steve" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+
+  user_data_replace_on_change = true
 
   tags = {
     Name = "terraform-example"
   }
+}
+resource "aws_security_group" "instance" {
+  name = var.security_group_name
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#INPUT VARIABLES
+variable "security_group_name" {
+  description = "The name of the security group"
+  type        = string
+  default     = "terraform-example-instance"
+}
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+#OUTPUT VARIABLES
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP of the Instance"
 }
